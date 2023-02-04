8a3d488e273e929af27e657ddb6c9aa2e7b987cf
Shafiudeen Bala <shafiudeen_b@outlook.com>
2023-02-03 21:47:42 -0600

    Created azure function api

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionApi = 'http://localhost:7071/api/FetchResumeCounter';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        count = response.count;
+        console.log("Website called function API.");
+        document.getElementById("counter").innerText = count;   
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
+
+    
