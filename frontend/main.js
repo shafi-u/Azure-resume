e4f9db0e24352046a71374ce3876e8350dd69b75
Shafiudeen Bala <shafiudeen_b@outlook.com>
2023-02-04 19:32:46 -0600

    Updated main.js with functionApiUrl

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/FetchResumeCounter';
+const functionApiUrl = 'https://fetchvisitorcounter.azurewebsites.net/api/FetchVisitorCounter?code=mSIhZWu_prH22lB2UwPrf_wEvLlD1ZeSYClt-pVdIkvcAzFuw8xP6w=='
+const localfunctionApi = 'http://localhost:7071/api/FetchVisitorCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         count = response.count;
