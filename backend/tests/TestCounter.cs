1fa2fb7d3b4f9d7ec18f125394ba119e8023067a
Shafiudeen Bala <shafiudeen_b@outlook.com>
2023-02-15 22:56:47 -0600

    Updaated testcounter

--- backend/tests/TestCounter.cs
+++ backend/tests/TestCounter.cs
@@ -4,11 +4,11 @@ using Xunit;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace tests
 {
@@ -17,15 +17,14 @@ namespace tests
         private readonly ILogger logger = TestFactory.CreateLogger();
 
         [Fact]
-        public async void Http_trigger_should_return_known_string()
+        public void Http_trigger_should_return_known_string()
         {
             var counter = new api.Counter();
             counter.Id = "1";
             counter.Count = 2;
             var request = TestFactory.CreateHttpRequest();
-            var response = (HttpResponseMessage) api.FetchVisitorCounter.Run(request, counter, out counter, logger);
+            var response = (HttpResponseMessage)api.FetchVisitorCounter.Run(request, counter, out counter, logger);
             Assert.Equal(3, counter.Count);
         }
-
     }
 }
